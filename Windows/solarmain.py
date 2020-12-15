340f7ab08be6b35173e5caa705c219a800c4441a
Daniel Kennedy <46408317+NeptuniteDaniel@users.noreply.github.com>
2020-12-15 13:50:29 +0000

    Saved servers menu, update checking and logging
    Added saved servers menu, update checking and logging.

--- Windows/solarmain.py
+++ Windows/solarmain.py
@@ -1,123 +1,185 @@
+
 # SOLAR LAUNCHER - UNOFFICIAL LAUNCHER FOR PLANET GAME (github.com/Jachdich/planet-client, github.com/Jachdich/planet-server)
 # THIS VERSION ONLY WORKS ON WINDOWS
 
-import tkinter as tk
-import webbrowser as web
-import urllib.request
-from tkinter import ttk
-from ttkthemes import ThemedTk
-from tkinter import filedialog
-import os
-import configparser
-from tkinter import messagebox
-import feedparser
-import subprocess
-import sys
+SOLAR_VERSION = 1.0
 
-config = configparser.ConfigParser()
-config['SETUP'] = {}
-config['STYLE'] = {}
-config.read('solar.launchersettings')
-theme = config['STYLE']['currenttheme']
+import urllib.request, os, configparser, feedparser, subprocess, sys, logging, datetime, re
+import solarservers
+import tkinter as tk
+from tkinter import ttk, filedialog, font, messagebox
+import webbrowser as web
+from ttkthemes import ThemedTk
+
+#Setting up logging
+def LogFormat(text):
+    now = datetime.datetime.now()
+    current_time = now.strftime("%d/%m/%y, %H:%M:%S: ")
+    logging.info(current_time +text)
+    print(current_time +text)
+logging.basicConfig(filename="log.solar", encoding="utf-8", level=logging.DEBUG)
 
 #Translate theme names into actual stuff that can be used
-if theme == "Windows":
-    ttktheme = "vista"
-    bgdefault = "#F0F0F0"
-    fgdefault = "#000000"
-    print("Using Windows theme:")
-elif theme == "Light":
-    bgdefault = "#F5F6F7"
-    ttktheme = "arc"
-    fgdefault = "#000000"
-    print("Using Light theme:")
-elif theme == "Dark":
-    ttktheme = "equilux"
-    bgdefault = "#464646"
-    fgdefault = "#a6a6a6"
-    print("Using Dark theme:")
-else:
-    ttktheme = "equilux"
-    bgdefault = "#464646"
-    fgdefault = "#a6a6a6"
-    print("Invalid theme name. Using default dark theme.")
-
-print(bgdefault +" " +fgdefault +" " +ttktheme)
+config = configparser.ConfigParser()
+config.read("themes.solar")
+theme = config["CONFIG"]["currenttheme"]
+bgdefault = config[theme]["background"]
+fgdefault = config[theme]["foreground"]
+ttktheme = config[theme]["ttktheme"]
+LogFormat("Using theme: " +theme +" " +bgdefault +" " +fgdefault +" " +ttktheme)
 
 #Configuring tkinter window
 window = ThemedTk(theme = ttktheme)
 window.title("Solar Launcher")
-window.geometry("400x350")
+window.geometry("480x400")
 icon = tk.PhotoImage(file = "icon.gif")
 window.iconphoto(True, icon)
 window.configure(bg = bgdefault)
-
 menubar = tk.Menu(window)
 window.config(menu = menubar)
 
-def SetClientLocation():
-    clientlocation = str(filedialog.askopenfilename())
-    config['SETUP']['ClientLocation'] = clientlocation
-    with open('solar.launchersettings', 'w') as configfile:
-        config.write(configfile)
-    if "planet-client.exe" not in clientlocation:
-        tk.messagebox.showwarning(title = "Solar Launcher - Warning", message = "This install location may be invalid. Proceed with caution.")
-    return
+class SetLocation:
+    def __init__(self, serverclient):
+        self.serverclient = serverclient
 
-def SetServerLocation():
-    serverlocation = str(filedialog.askopenfilename())
-    config['SETUP']['ServerLocation'] = serverlocation
-    with open('solar.launchersettings', 'w') as configfile:
-        config.write(configfile)
-    return
+    def SetFileLocation(self):
+        #Opens window to select planet-client location
+        location = str(filedialog.askopenfilename())
+        if self.serverclient == 'client':
+            config["SETUP"]["ClientLocation"] = location
+        else:
+            config["SETUP"]["ServerLocation"] = location
+        with open("settings.solar", "w") as configfile:
+            config.write(configfile)
+        if "planet" not in location:
+            tk.messagebox.showwarning(title = "Solar Launcher - Warning", message = "This install location may be invalid. Proceed with caution.")
 
 def InstallLocationWarning():
-    locationwarning = tk.messagebox.askyesno(title = 'Solar Launcher', message = 'Solar Launcher has detected an invalid or missing install location for planet-client. Would you like to set a new one now?')
-    if locationwarning == True:
-        SetClientLocation()
+    config.read("settings.solar")
+    clientsavedlocation = (config["SETUP"]["clientlocation"])
+    LogFormat("Install Location: " +clientsavedlocation)
+    if "planet-client.exe" not in str(clientsavedlocation):
+        locationwarning = tk.messagebox.askyesno(title = "Solar Launcher", message = "Solar Launcher has detected an invalid or missing install location for planet-client. Would you like to set a new one now?")
+        if locationwarning == True:
+            setclientlocation.SetFileLocation()
     return
 
 def LoadUpdateFeed():
-    feed = feedparser.parse('https://github.com/Jachdich/planet-client/commits/master.atom')
-    feedtext = (feed['entries'][0]['title'])
-    feedlabel.config(text = feedtext)
-    updatetime = "at " +feed['entries'][0]['updated']
-    updatelabel.config(text = updatetime)
-    return
+    try:
+        feed = feedparser.parse("https://github.com/Jachdich/planet-client/commits/master.atom")
+        feedtext = (feed["entries"][0]["title"])
+        updatetime = "at " +feed["entries"][0]["updated"]
+        LogFormat("Commit feed updated successfully.")
+    except urllib.error.URLError:
+        feedtext = "Failed to connect to GitHub."
+        updatetime = "Please check your internet connection."
+        LogFormat("Cannot connect to GitHub. Will not update feed.")
+    try:
+        feedlabel.config(text = feedtext)
+        updatelabel.config(text = updatetime)
+        return
+    except NameError:
+        return feedtext, updatetime
 
 def LaunchClient():
-    clientsavedlocation = (config['SETUP']['clientlocation'])
-    launchcommand = clientsavedlocation +" " +ipentry.get()
+    #Launches planet-client with given install location and IP
+    clientsavedlocation = (config["SETUP"]["clientlocation"])
+    launchcommand = clientsavedlocation +" " +ipaddress.get()
     print("Opening " +launchcommand)
-    subprocess.call(launchcommand)
-    window.destroy()
+    try:
+        subprocess.call(launchcommand)
+        window.destroy()
+    except FileNotFoundError:
+        tk.messagebox.showerror(title = "Solar Launcher - Error", message = "No planet-client install location specified. Please set a location under the Setup menu tab.")
 
 def ChangeTheme(t):
-    config['STYLE']['currenttheme'] = t
-    with open('solar.launchersettings', 'w') as configfile:
+    config["STYLE"]["currenttheme"] = t
+    with open("settings.solar", "w") as configfile:
         config.write(configfile)
     tk.messagebox.showwarning(title = "Solar Launcher", message = "Please restart Solar Launcher for changes to take effect.")
     return
 
 def OpenOptionsMenu(s):
+    #Opens the options menu window
     optionsmenu = tk.Toplevel(window, bg = bgdefault)
-    optionsmenu.geometry('400x300')
-    optionstitle = ttk.Label(optionsmenu, text = "Options", font = 'Helvetica 20').pack()
+    optionsmenu.geometry("400x300")
+    optionstitle = ttk.Label(optionsmenu, text = "Options", font = "Helvetica 20").pack()
     divider = ttk.Label(optionsmenu, text = " ", font = "Unispace 10 bold").pack()
 
-    appearancetitle = ttk.Label(optionsmenu, text = 'Appearance Settings', font = 'Helvetica 10 underline').pack()
-    stylelabel = ttk.Label(optionsmenu, text = 'Theme: ').pack(anchor = tk.W, padx = 10)
+    appearancetitle = ttk.Label(optionsmenu, text = "Appearance Settings", font = "Helvetica 10 underline").pack()
+    stylelabel = ttk.Label(optionsmenu, text = "Theme: ").pack(anchor = tk.W, padx = 10, pady = 8)
 
     stylevariable = tk.IntVar()
-    if s == "Dark":
+    if s == "DARK":
         stylevariable.set(1)
-    elif s == "Light":
+    elif s == "LIGHT":
         stylevariable.set(2)
-    elif s == "Windows":
-        stylevariable.set(3)
-    ttk.Radiobutton(optionsmenu, text = "Dark", variable = stylevariable, value = 1, command = (lambda t = 'Dark': ChangeTheme(t))).pack(anchor = tk.W)
-    ttk.Radiobutton(optionsmenu, text = "Light", variable = stylevariable, value = 2, command = (lambda t = 'Light': ChangeTheme(t))).pack(anchor = tk.W)
-    ttk.Radiobutton(optionsmenu, text = "Windows", variable = stylevariable, value = 3, command = (lambda t = 'Windows': ChangeTheme(t))).pack(anchor = tk.W)
+    ttk.Radiobutton(optionsmenu, text = "Dark", variable = stylevariable, value = 1, command = (lambda t = "DARK": ChangeTheme(t))).pack(anchor = tk.W, padx = 20, pady = 2)
+    ttk.Radiobutton(optionsmenu, text = "Light", variable = stylevariable, value = 2, command = (lambda t = "LIGHT": ChangeTheme(t))).pack(anchor = tk.W, padx = 20, pady = 2)
+
+class SavedServersMenu:
+    def __init__(self, window):
+        self.window = window
+        try:
+            self.servernames = solarservers.manageservers.ServerSearch()
+            self.serverdetails = solarservers.manageservers
+        except TypeError:
+            self.servernames = {"None":""}
+        self.windowopen = False
+
+    def InsertSavedServer(self):
+        self.solargetdetails = solarservers.ManageServers(self.selectedserver.get())
+        ipaddress.set(self.solargetdetails.GetServerDetails()["ip"])
+
+    def SaveServer(self):
+        self.serversconfig = solarservers.ManageServers(self.newservernametext.get(), self.newserveriptext.get())
+        self.serversconfig.AddNewServer()
+
+    def CreateWindow(self):
+        serversmenu = tk.Toplevel(self.window, bg = bgdefault)
+        serversmenu.geometry("280x240")
+        self.selectedserver = tk.StringVar()
+        self.serversselect = ttk.OptionMenu(serversmenu, self.selectedserver, "Select a saved server...", *self.servernames)
+        self.serversselect.pack(pady = 10)
+        self.applyaddress = ttk.Button(serversmenu, text = "Apply", command = self.InsertSavedServer).pack()
+        ttk.Label(serversmenu, text = "Add a new saved server:").pack(pady = 10, padx = 5, anchor = tk.W)
+
+        self.newservernameframe = tk.Frame(serversmenu, bg = bgdefault)
+        self.newservernameframe.pack(pady = 5)
+        ttk.Label(self.newservernameframe, text = "Server Name:").pack(padx = 10, pady = 5, side = tk.LEFT)
+        self.newservernametext = tk.StringVar()
+        self.newservername = ttk.Entry(self.newservernameframe, textvariable = self.newservernametext)
+        self.newservername.pack(side = tk.RIGHT)
+
+        self.newserveripframe = tk.Frame(serversmenu, bg = bgdefault)
+        self.newserveripframe.pack(pady = 5)
+        ttk.Label(self.newserveripframe, text = "Server Address:").pack(padx = 10, pady = 5, side = tk.LEFT)
+        self.newserveriptext = tk.StringVar()
+        self.newserverip = ttk.Entry(self.newserveripframe, textvariable = self.newserveriptext)
+        self.newserverip.pack(side = tk.RIGHT)
+
+        ttk.Button(serversmenu, text = "Save Server", command = self.SaveServer).pack(pady = 5)
+
+
+def UpdateCheck():
+    try:
+        releasefeed = feedparser.parse("https://github.com/NeptuniteDaniel/SolarLauncher/releases.atom")
+        versionname = (releasefeed["entries"][0]["title"])
+        versionnumber = re.findall("\d+\.\d+", versionname)
+        latestversion = versionnumber[0]
+        versioninfo = ("Current version: " +str(SOLAR_VERSION) +" Latest: " +str(latestversion))
+        LogFormat(versioninfo)
+        if float(latestversion) > float(SOLAR_VERSION):
+            updatenotify = tk.messagebox.askyesno(title = "Solar Launcher - Update", message = ("There is a newer version of Solar Launcher available. Would you like to download it now?" +" (" +str(latestversion) +" > " + str(SOLAR_VERSION) +")"))
+            if updatenotify == True:
+                web.open("github.com/NeptuniteDaniel/SolarLauncher")
+        return versioninfo
+    except urllib.error.URLError or IndexError:
+        LogFormat("Update checking could not find or connect to a version list.")
+        return "Could not find or connect to a version list."
+
+setclientlocation = SetLocation('client')
+setserverlocation = SetLocation('server')
 
 #About Menu
 aboutmenu = tk.Menu(menubar)
@@ -128,50 +190,48 @@ aboutmenu.add_command(label = "planet-server on GitHub", command = (lambda l = "
 #Setup Menu
 setupmenu = tk.Menu(menubar)
 menubar.add_cascade(label = "Setup", menu = setupmenu)
-setupmenu.add_command(label = "Set planet-client install location", command = SetClientLocation)
-setupmenu.add_command(label = "Set planet-server install location", command = SetServerLocation)
+
+setupmenu.add_command(label = "Set planet-client install location", command = setclientlocation.SetFileLocation)
+setupmenu.add_command(label = "Set planet-server install location", command = setserverlocation.SetFileLocation)
 
 #Options Menu
 optionsmenu = tk.Menu(menubar)
 menubar.add_cascade(label = "Options", menu = optionsmenu)
-optionsmenu.add_command(label = "Refresh commit updates", command = LoadUpdateFeed)
 optionsmenu.add_command(label = "Options menu", command = (lambda s = theme: OpenOptionsMenu(s)))
 
 #Title
-title = ttk.Label(text = "Solar Launcher", font = "GENISO 40").pack()
-title2 = ttk.Label(text = "Unofficial launcher for Planet Game", font = "Helvetica 12").pack()
-divider = ttk.Label(text = " ", font = "Unispace 10 bold").pack()
+ttk.Label(text = "Solar Launcher", font = "GENISO 40").pack()
+ttk.Label(text = "Unofficial launcher for Planet Game", font = "Helvetica 12").pack()
 
 #Update Feed
 updateframe = tk.Frame(window, highlightbackground = fgdefault, highlightthickness = 1, bg = bgdefault)
-updateframe.pack()
+updateframe.pack(pady = 10)
 
-feedtitle = ttk.Label(updateframe, text = "Most recent commit to planet-client master:", font = "Helvetica 10 bold").pack()
-dividersmall = ttk.Label(updateframe, text = " ", font = "Unispace 2 bold").pack()
+ttk.Label(updateframe, text = "Most recent commit to planet-client master:", font = "Helvetica 10 bold").pack()
+ttk.Label(updateframe, text = " ", font = "Unispace 2 bold").pack()
 
-feed = feedparser.parse('https://github.com/Jachdich/planet-client/commits/master.atom')
-feedtext = (feed['entries'][0]['title'])
+feedtext, updatetime = LoadUpdateFeed()
+
 feedlabel = ttk.Label(updateframe, text = feedtext)
 feedlabel.pack()
-updatetime = "at " +feed['entries'][0]['updated']
 updatelabel = ttk.Label(updateframe, text = updatetime)
 updatelabel.pack()
-dividersmall = ttk.Label(updateframe, text = " ", font = "Unispace 2 bold").pack()
-updatebutton = ttk.Button(updateframe, text = "Download new updates", command = (lambda l = "https://github.com/Jachdich/planet-client/commits": web.open(l))).pack()
-divider = ttk.Label(text = " ", font = "Unispace 10 bold").pack()
+ttk.Button(updateframe, text = "Download new updates", command = (lambda l = "https://github.com/Jachdich/planet-client/commits": web.open(l))).pack(pady = 10)
+optionsmenu.add_command(label = "Refresh commit updates", command = LoadUpdateFeed)
 
 #IP Input
-iptext = ttk.Label(text = "Server Address:").pack()
-ipentry = ttk.Entry()
-ipentry.pack()
-divider = ttk.Label(text = " ", font = "Unispace 10 bold").pack()
-ipaddress = str(ipentry.get)
-launchbutton = ttk.Button(text = "Connect", command = LaunchClient).pack()
+ttk.Label(text = "Server Address:").pack()
+ipaddress = tk.StringVar()
+ipentry = ttk.Entry(textvariable = ipaddress)
+ipentry.pack(pady = 5)
+versionlabel = ttk.Label(text = "Getting version...", font = 'Helvetica 8')
+versionlabel.pack(side = tk.BOTTOM)
+ttk.Button(text = "Connect", command = LaunchClient).pack(side = tk.BOTTOM, pady = 10)
+createsavedservers = SavedServersMenu(window)
+savedserversbutton = ttk.Button(text = "Saved Servers", command = (createsavedservers.CreateWindow))
+savedserversbutton.pack(side = tk.TOP)
 
-config.read('solar.launchersettings')
-clientsavedlocation = (config['SETUP']['clientlocation'])
-print("Install Location: " +clientsavedlocation)
-if "planet-client.exe" not in str(clientsavedlocation):
-    InstallLocationWarning()
+versionlabel.config(text = UpdateCheck())
+InstallLocationWarning()
 
 window.mainloop()
